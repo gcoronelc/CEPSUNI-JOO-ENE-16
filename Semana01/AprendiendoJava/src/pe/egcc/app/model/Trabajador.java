286538283beed31c914e661b783d361e3cad4333
Master <Master>
2016-01-08 19:32:13 -0500

    Actualizando Semana01

---
+++ Semana01/AprendiendoJava/src/pe/egcc/app/model/Trabajador.java
@@ -0,0 +1,18 @@
+package pe.egcc.app.model;
+
+/**
+ *
+ * @author Eric Gustavo Coronel Castillo
+ * @site   www.desarrollasoftware.com
+ * @date   08/01/2016
+ * @email  gcoronelc@gmail.com
+ */
+public class Trabajador {
+  
+  private String colorOjos;
+  private String nombre;
+  private String apellido;
+  
+  
+    
+}
